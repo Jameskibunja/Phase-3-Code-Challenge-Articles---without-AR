7f695281276dc03cdbed450a3d64b1d256adaf71
JAMES <jameskibunja17@gmail.com>
2023-03-22 14:17:37 +0300

    Done Challenge

--- solution.rb
+++ solution.rb
@@ -1,1 +1,96 @@
 # Please copy/paste all three classes into this file to submit your solution!
+# Please copy/paste all three classes into this file to submit your solution!
+class Author
+    attr_reader :name
+  
+    def initialize(name)
+      # An author is initialized with a name, as a string.
+      # A name cannot be changed after it is initialized.
+      @name = name
+    end
+  
+    # Returns the name of the author
+    def name
+      @name
+    end
+  
+    # Returns an array of Article instances the author has written
+    def articles
+      Article.all.select { |article| article.author == self }
+    end
+  
+    # Returns a unique array of Magazine instances for which the author has contributed to
+    def magazines
+      articles.map(&:magazine).uniq
+    end
+  
+    # Given a magazine (as Magazine instance) and a title (as a string),
+    # creates a new Article instance and associates it with that author and that magazine.
+    def add_article(magazine, title)
+      Article.new(self, magazine, title)
+    end
+  
+    # Returns a unique array of strings with the categories of the magazines the author has contributed to
+    def topic_areas
+      magazines.map(&:category).uniq
+    end
+  end
+  
+  class Magazine
+    attr_accessor :name, :category
+    @@all = []
+  
+    def initialize(name, category)
+      # A magazine is initialized with a name as a string and a category as a string
+      @name = name
+      @category = category
+      @@all << self
+    end
+  
+    def self.all
+      # Returns an array of all Magazine instances
+      @@all
+    end
+  
+    # Returns an array of Author instances who have written for this magazine
+    def contributors
+      articles = Article.all.select { |article| article.magazine == self }
+      articles.map(&:author).uniq
+    end
+  
+    # Given a string of magazine's name, this method returns the first magazine object that matches
+    def self.find_by_name(name)
+      all.find { |magazine| magazine.name == name }
+    end
+  
+    # Returns an array strings of the titles of all articles written for that magazine
+    def article_titles
+      articles = Article.all.select { |article| article.magazine == self }
+      articles.map(&:title)
+    end
+  
+    # Returns an array of authors who have written more than 2 articles for the magazine
+    def contributing_authors
+      contributors.select { |author| author.articles.count { |article| article.magazine == self } > 2 }
+    end
+  end
+  
+  class Article
+    attr_reader :author, :magazine, :title
+    @@all = []
+  
+    def initialize(author, magazine, title)
+      # An article is initialized with an author as an Author object,
+      # a magazine as a Magazine object, and title as a string.
+      @author = author
+      @magazine = magazine
+      @title = title
+      @@all << self
+    end
+  
+    def self.all
+      # Returns an array of all Article instances
+      @@all
+    end
+  end
+  
--- tools/console.rb
+++ tools/console.rb
@@ -4,18 +4,57 @@ def reload
   load 'config/environment.rb'
 end
 
-
 ###  WRITE YOUR TEST CODE HERE ###
 
+require_relative './solution.rb'
 
+# Creating sample authors
+author1 = Author.new("Alice")
+author2 = Author.new("Bob")
+author3 = Author.new("Charlie")
 
+# Creating sample magazines
+magazine1 = Magazine.new("Tech Today", "Technology")
+magazine2 = Magazine.new("Nature Journal", "Environment")
+magazine3 = Magazine.new("Sci World", "Science")
 
+# Adding articles to authors and magazines
+author1.add_article(magazine1, "AI Revolution")
+author1.add_article(magazine2, "Climate Change Impact")
+author2.add_article(magazine1, "Blockchain Technology")
+author2.add_article(magazine2, "Sustainable Energy")
+author2.add_article(magazine3, "Quantum Computing")
+author3.add_article(magazine3, "Mars Exploration")
 
+# Test cases
+puts "===== Author#name ====="
+puts author1.name == "Alice"
+puts author2.name == "Bob"
 
+puts "===== Author#articles ====="
+puts author1.articles.size == 2
+puts author2.articles.size == 3
 
+puts "===== Author#magazines ====="
+puts author1.magazines.size == 2
+puts author2.magazines.size == 3
 
+puts "===== Author#topic_areas ====="
+puts author1.topic_areas == ["Technology", "Environment"]
+puts author2.topic_areas == ["Technology", "Environment", "Science"]
 
-### DO NOT REMOVE THIS
-binding.pry
+puts "===== Magazine#contributors ====="
+puts magazine1.contributors.size == 2
+puts magazine2.contributors.size == 2
 
-0
+puts "===== Magazine.find_by_name ====="
+puts Magazine.find_by_name("Tech Today") == magazine1
+puts Magazine.find_by_name("Nature Journal") == magazine2
+
+puts "===== Magazine#article_titles ====="
+puts magazine1.article_titles == ["AI Revolution", "Blockchain Technology"]
+puts magazine2.article_titles == ["Climate Change Impact", "Sustainable Energy"]
+
+puts "===== Magazine#contributing_authors ====="
+puts magazine1.contributing_authors.empty?
+puts magazine2.contributing_authors.empty?
